6737152892334b76588452d9ae45d00f49e7f0cb
Luis Llerena <luis.llerenaherraez@gmail.com>
2025-06-07 07:54:37 +0200

    create controller autores

---
+++ src/controllers/autores.controllers.js
@@ -0,0 +1,25 @@
+const autores = require ('../models/autores.model');
+
+const getAll = async (req, res) => {
+
+}
+
+const getById = async (req, res) => {
+
+}
+
+const create = async (req, res) => {
+
+}
+
+const deleteById = async (req, res) => {
+
+}
+
+
+const updDateById = async (req, res) => {
+
+}
+
+
+module.exports = { getAll, getById, create, deleteById, updDateById}
