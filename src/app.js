15f43f35c127f225b8696a3409da17ad098727cb
Luis Llerena <luis.llerenaherraez@gmail.com>
2025-06-11 16:59:02 +0200

    app.js

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+// 404 handler
+app.use((req, res, next) => {
+    res.status(404).json({
+        message: "Not found"
+    });
+});
+
+// Error handler
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: err.message });
+});
+
+module.exports = app;
