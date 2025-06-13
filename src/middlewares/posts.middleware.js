9c75f2a98b7d3ce58cbcd7b953b3f0b4f64c2115
Luis Llerena <luis.llerenaherraez@gmail.com>
2025-06-13 21:17:04 +0200

    Create middleware post

---
+++ src/middlewares/posts.middleware.js
@@ -0,0 +1,17 @@
+const posts = require ('../models/posts.model');
+
+const checkPostId = async (req, res, next) => {
+    const  id = req.query.id ? req.query.id : req.params.id;
+
+    const noEsNumero = isNaN(id)
+    if(noEsNumero) {
+        return res.status(404).json({errorMsg: 'El id no es un número'});
+    }
+    const post = await posts.getById(id)
+        if ( post === null) {
+        return res.status(404).json({errorMsg: 'El post no existe'});      
+    }
+
+    next();
+}
+module.exports = {checkPostId}
